900c1d5495322060a7ca13d6a5e57c7171439ee0
deboracorrales <deboramailen88@gmail.com>
2021-04-10 16:15:07 -0300

    Guia ejercicio 04
    primer set de cambios ej 04

---
+++ CAI_ej.04/CAI_ej.04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_ej._04
+//Ej 4: Permita el ingreso de frases, finalizando cuando el usuario ingresa “fin”.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string texto;
+			do
+			{
+				Console.WriteLine("Ingrese una frase. Escriba FIN para finalizar.");
+				texto = Console.ReadLine();
+
+				/*if(texto == "fin")
+				{
+					Console.WriteLine(" Programa finalizado");
+					Console.ReadLine();
+				}*/
+
+			} while (texto != "fin");
+			Console.WriteLine(texto);
+			Console.ReadKey();
+
+		}
+	}
+}
